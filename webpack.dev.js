353e079e14447b75e5517874048745d54248e89d
zrtg <[thomas4stank@gmail.com]>
2024-12-18 04:09:31 +0100

    Added prettier

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,10 @@
-
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-	mode: "development",
-        devtool: "eval-source-map",
-	devServer: {
-		watchFiles: ["./src/template.html"],
-	},
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
 });
